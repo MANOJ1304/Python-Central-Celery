425dc10d4462468282eb438f1326897af409f3c7
veeresh <veereshp@intelipower.co.za>
2019-03-12 20:19:59 +0530

    Iviu fix continuous run

--- celery_app/tasks/iviu_connector_nest/run_sql_db.py
+++ celery_app/tasks/iviu_connector_nest/run_sql_db.py
@@ -4,6 +4,7 @@ from datetime import datetime
 import os
 import json
 import time
+import logging
 import redis
 import yaml
 import pymysql.cursors
@@ -11,7 +12,6 @@ from celery import Celery
 from tasks.celery_queue_tasks import ZZQHighTask
 from tasks.iviu_connector_nest.net_connection import CheckNet
 from celery.task.control import revoke
-
 
 config_file = os.path.join(
     os.path.dirname(os.path.abspath(__file__)),
@@ -21,7 +21,6 @@ with open(config_file, 'r') as ymlfile:
         cfg = yaml.load(ymlfile)
 redis_channel = cfg['redis_channel_events']
 
-
 class IviuConnect(ZZQHighTask):
     """ testing Task. """
     name = 'Iviu Connector'
@@ -30,6 +29,10 @@ class IviuConnect(ZZQHighTask):
     autoinclude = True
 
     db = Database()
+
+    def __init__(self):
+        logging.basicConfig(level=logging.INFO)
+        self.logger = logging.getLogger(__name__)
 
     def run(self, *args, **kwargs):
         """celery initialise function from here."""
@@ -55,31 +58,23 @@ class IviuConnect(ZZQHighTask):
             self.thread_each_table(args[0], "")
 
 
-
     def start(self):
         """main entry point for db connection."""
         try:
             if self.net.check_connection(cfg['other']['connection']):
                 redis_conf = self.config_json['redis_connection']
                 for i in list(redis_conf):
-                    if 'password' not in redis_conf[i]:
-                        self.redis_connections[i] = redis.StrictRedis(
-                                host=redis_conf[i]['host'],
-                                port=redis_conf[i]['port'],
-                                socket_keepalive=True)
-                        # print('redis_connections1 --- {}'.format(self.redis_connections))
-                    else :
-                        self.redis_connections[i] = redis.StrictRedis(
-                                host=redis_conf[i]['host'],
-                                port=redis_conf[i]['port'],
-                                password = redis_conf[i]['password'],
-                                socket_keepalive=True)
-                        # print('redis_connections2 --- {}'.format(self.redis_connections))
+                    self.redis_connections[i] = redis.StrictRedis(
+                            host=redis_conf[i]['host'],
+                            port=redis_conf[i]['port'],
+                            password = redis_conf[i]['password'] if "password" in redis_conf[i] else "",
+                            socket_keepalive=True)
+                    # print('redis_connections2 --- {}'.format(self.redis_connections))
 
                 self.redis_conn = redis.StrictRedis(
                     host=self.config_json['redis_err']['host'],
                     port=self.config_json['redis_err']['port'],
-                    password=self.config_json['redis_err']['password'],
+                    password=self.config_json['redis_err']['password'] if "password" in self.config_json['redis_err'] else "",
                     socket_keepalive=True)
         except (redis.ConnectionError,redis.TimeoutError) as identifier:
             pass
@@ -93,7 +88,7 @@ class IviuConnect(ZZQHighTask):
             db=self.config_json["mysql"]["db"],
             )
         self.db.generate_mapping(create_tables=True)
-        # set_sql_debug(True)
+        set_sql_debug(False)
 
     class IviuEntity(db.Entity):
         # _table_ = 'p00169'
@@ -120,12 +115,16 @@ class IviuConnect(ZZQHighTask):
         fs = Required(int)
         utcft = Required(datetime)
         it = Required(datetime)
+        # unix_tt = Optional(int)
 
-    def post_to_redis(self, channel, post_data):
+    def post_to_redis(self,tt,table_name, post_data):
         try:
             redis_conf = self.config_json['redis_connection']
             for i in list(redis_conf):
-                self.redis_connections[i].lpush(channel,post_data)
+                # print("getting tt and tablename",redis_channel,self.redis_connections[i],type(post_data))
+                # channel = redis_channel+table_name
+                # self.redis_connections[i].zadd(channel,{post_data: tt})
+                self.redis_connections[i].lpush(redis_channel,post_data)
         except Exception as ex:
             # print ('Error:', ex)
             exit('Failed to connect, terminating.')
@@ -138,13 +137,17 @@ class IviuConnect(ZZQHighTask):
         # for i in cfg['other']['pathDict'].items():
         #     dpath.util.set(base,i[1],iviu[i])
         iviu_format = dict(self.temp_iviu)
-        iviu_format['data'] = iviu
 
         if table_name == "p00390_TES":
             table_name = "p00390"
         iviu_format['table'] = table_name
 
-        self.post_to_redis(redis_channel,json.dumps(iviu_format, default = self.myconverter))
+        #Assign unix time
+        # iviu['tt'] = iviu['unix_tt']
+        iviu_format['data'] = iviu
+        # print("hash value of each row {}------".format(iviu_format))
+
+        self.post_to_redis(iviu['tt'],table_name,json.dumps(iviu_format, default = self.myconverter))
 
     def handle_er(self,net,tableName,tt):
         data_err = { "alarm_message": "iviu_connector_error",
@@ -166,6 +169,7 @@ class IviuConnect(ZZQHighTask):
     @db_session()
     def thread_each_table(self, i, tt=""):
         # print("Attributes ---{}".format(dir(self.app.request)))
+        self.logger.info('Start reading database')
         tableName = i
         # print("tt in thread_each_table method",tt)
         # print('table_name', i)
@@ -177,33 +181,36 @@ class IviuConnect(ZZQHighTask):
         while(iviu_process):
             whereClause = ""
             if len(tt) != 0:
-                whereClause = " WHERE tt > '" + tt + "'"
-            # sqlQuery = "SELECT * FROM iViu_data." + tableName + whereClause + " order by tt ASC LIMIT " + str(limit)
+                whereClause = " WHERE tt > '" + tt + "' AND inv !='0.0'"
             sqlQuery = "SELECT * FROM " + self.config_json['mysql']['db'] + "." + tableName + whereClause + " order by tt ASC LIMIT " + str(limit)
+            # sqlQuery = "SELECT *,CAST( (UNIX_TIMESTAMP(tt) * 1000) AS UNSIGNED) as unix_tt FROM " + self.config_json['mysql']['db'] + "." + tableName + whereClause + " order by tt ASC LIMIT " + str(limit)
 
             query=None
             try:
                 query = self.IviuEntity.select_by_sql(sqlQuery)
                 iviu_list = list(query)
-                record_id = []
+                # record_id = []
                 for f in iviu_list:
                     # print("Timestamp:{}{}".format(f.tt, tableName) )
                     tt = f.tt.__str__()
-                    record_id.append(f.id.__str__())
+                    # record_id.append(f.id.__str__())
                     self.formatToConnector(f.to_dict(),tableName)
                     offset += limit
                 # for row_id in record_id:
                     # print("row_id",row_id)
                     # self.IviuEntity.select(lambda iv: iv.id == row_id).delete(bulk=True)
-                record_id = []
+                # record_id = []
                 # rowcount += 1
+                self.db.rollback()
             except Exception as ex:
                 iviu_process = False
                 self.handle_er(net,tableName,tt)
                 # print("exception in tread create table ",ex)
+                self.logger.error('{} table -- {} raised an error'.format(tableName,ex))
         else:
             self.db.rollback()
             # revoke(self.task_id, terminate=True)
             # print("process stopped for table -- {}, task id -- {}".format(tableName, self.task_id))
+            self.logger.error('{} table raised an error'.format(tableName))
             time.sleep(0.001)
         self.db.rollback()
