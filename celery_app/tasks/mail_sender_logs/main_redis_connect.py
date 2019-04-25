fb69aa2c9303c95c7f8e660201baa604d231d054
Manmohan Singh <manmohans@intelipower.co.za>
2019-04-25 19:30:47 +0530

    modify files

--- celery_app/tasks/mail_sender_logs/main_redis_connect.py
+++ celery_app/tasks/mail_sender_logs/main_redis_connect.py
@@ -41,14 +41,14 @@ class FetchRedisRecords(ZZQLowTask):
         pubsub.subscribe(self.config_json["redis_connect"]["redis_key"])
         cnt = 1
         email_list = os.getenv("mail_alert_list")
-        # if email_list is None:
-        #     print("Alert! email list not found. entering default emails.")
-        #     os.environ["mail_alert_list"] = (
-        #         "[\"sushil.jaiswar@tes.media\""
-        #         ",\"roger.blain@tes.media\"]"
-        #     )
-        #     email_list = os.getenv("mail_alert_list")
-        #     print('...new email_list...', email_list)
+        if email_list is None:
+            print("Alert! email list not found. entering default emails.")
+            os.environ["mail_alert_list"] = (
+                "[\"sushil.jaiswar@tes.media\""
+                ",\"roger.blain@tes.media\"]"
+            )
+            email_list = os.getenv("mail_alert_list")
+            print('...new email_list...', email_list)
         print("email list: {} \t.. {}".format(email_list, type(email_list)))
         while True:
             message = pubsub.get_message()
